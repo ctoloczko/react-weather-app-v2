6d6c65a7dcf22b186a6e393f42c66bb180f762e2
ctoloczko <celina.toloczko@gmail.com>
2024-08-18 10:31:35 +0200

    added Weather.css file

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./App.css";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <>
-      <div className="weather-app">
+      <div className="Weather">
         <div className="col d-flex ">
           <form className="d-flex">
             <input
@@ -15,34 +15,38 @@ export default function Weather() {
             <input className="search-button" type="submit" value="🔎" />
           </form>
         </div>
+        <div className="Column-header">
+          <p>August 18</p>
 
-        <p>August 18</p>
+          <div className="d-flex justify-content-between">
+            {" "}
+            <p>Sunday</p>
+            <p>10:29 AM</p>
+          </div>
+        </div>
 
-        <div className="d-flex justify-content-between">
-          {" "}
-          <p>Sunday</p>
-          <p>10:29 AM</p>
-        </div>
-        <div>
-          <h3>Stockholm</h3>
-        </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <h2>16°C|F</h2>
-              <p>Feels like: 17° </p>
-              <p>Humidity: 42%</p>
-              <p>Wind: 23km/h</p>
-              <p>Pressure: 1200 MB</p>
-            </div>
-            <div className="col-md-6">
-              <h3>
-                {" "}
-                <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
-                  alt="weather-icon"
-                ></img>
-              </h3>
+        <div className="Column-body">
+          <div>
+            <h3>Stockholm</h3>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <h2>16°C|F</h2>
+                <p>Feels like: 17° </p>
+                <p>Humidity: 42%</p>
+                <p>Wind: 23km/h</p>
+                <p>Pressure: 1200 MB</p>
+              </div>
+              <div className="col-md-6">
+                <h3>
+                  {" "}
+                  <img
+                    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                    alt="weather-icon"
+                  ></img>
+                </h3>
+              </div>
             </div>
           </div>
         </div>
